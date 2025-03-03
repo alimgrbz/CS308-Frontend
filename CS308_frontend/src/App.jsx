d558b42adca5b9e23ac464413f9679b417878ed5
Alım Gürbüz <alimgurbuz@sabanciuniv.edu>
2025-03-03 19:59:14 +0300

    Initial commit

---
+++ CS308_frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div>
+      boş proje :D
+    </div>
+  )
+}
+
+export default App
