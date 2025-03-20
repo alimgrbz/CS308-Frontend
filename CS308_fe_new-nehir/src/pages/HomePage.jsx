4357e1822e3bbcc76daae524c1930e652fa8c633
nehir <silvianxeity@gmail.com>
2025-03-20 21:30:20 +0300

    EditedSignUp

---
+++ CS308_fe_new-nehir/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
+import '../styles/HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      
+
+    </div>
+  );
+};
+
+export default HomePage;
